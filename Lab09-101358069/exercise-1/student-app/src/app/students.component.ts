81cd545a61a351c0b7b030462b4ce4e2d3645d68
Noor Soreti <noor.said0698@gmail.com>
2023-03-30 18:34:19 -0400

    Lab09-101358069

---
+++ Lab09-101358069/exercise-1/student-app/src/app/students.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'students',
+    template: '<h2>{{ getTitle() }} {{ getCurrentDate() }}</h2>'
+})
+
+export class StudentsComponent {
+    title = "My List of Students";
+
+    getTitle() {
+        return this.title;
+    }
+
+    getCurrentDate() {
+        return new Date();
+    }
+
+}
